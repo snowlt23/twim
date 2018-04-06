bde155978b66eb73b300769ba5208fe826f626a6
snowlt23 <shsnow23@gmail.com>
2018-04-07 06:45:37 +0900

    add options page

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@ flori js twim.flori
 mv twim.js dist/twim.js
 flori js background.flori
 mv background.js dist/background.js
+flori js twim_options.flori
+mv twim_options.js dist/twim_options.js
 
+cp twim_options.html dist/twim_options.html
 cp manifest.json dist/manifest.json
+
 cp images/icon-enable.png dist/icon-enable.png
 cp images/icon-disable.png dist/icon-disable.png
--- develop.sh
+++ develop.sh
@@ -1,7 +1,11 @@
 
+
 flori js twim.flori
 mv twim.js dist/twim.js
 flori js background.flori
 mv background.js dist/background.js
+flori js twim_options.flori
+mv twim_options.js dist/twim_options.js
 
+cp twim_options.html dist/twim_options.html
 cp manifest.json dist/manifest.json
